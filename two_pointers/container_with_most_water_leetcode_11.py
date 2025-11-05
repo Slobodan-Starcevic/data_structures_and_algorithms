e329f25b89b9f414496c837c25769e2c50379347
Slobodan Starcevic <slobodanstarcevicode@gmail.com>
2025-11-05 22:28:26 +0100

    solved max area

---
+++ two_pointers/container_with_most_water_leetcode_11.py
@@ -0,0 +1,34 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        """
+        Problem analysis:
+        - Given int array height, for visualization we can think of an index as a wall
+        - 2 indeces forming a bucket, find the pair that can hold the most water, and return said amount
+
+        Constraints:
+        - n == height.length
+        - 2 <= n <= 105
+        - 0 <= height[i] <= 104
+
+        Edge-cases:
+        - All walls height 0
+
+        Approach 1:
+        - Converging two pointer approach
+
+        Time & Space Complexity (respectively):
+        - O(n/2), as we will be traversing half of the array (half rounded up)
+        - O(1), as we do not make changes to the original array
+        """
+        left, right = 0, len(height) - 1
+
+        max_water = 0
+        while left != right:
+            water = (min(height[left], height[right])) * (right - left)
+            if water > max_water:
+                max_water = water
+            if height[left] > height[right]:
+                right -= 1
+            else:
+                left += 1
+        return max_water
