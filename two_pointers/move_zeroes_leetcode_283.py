c06cdd4012f86aea0976d3d96e32e6417ed42cbe
Slobodan Starcevic <slobodanstarcevicode@gmail.com>
2025-11-05 20:59:21 +0100

    move zeroes solved

---
+++ two_pointers/move_zeroes_leetcode_283.py
@@ -0,0 +1,42 @@
+from typing import List
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Problem analysis:
+        - Given an array of integers
+        - Move all 0's to the right of the array
+        - Non-zero elements need to stay in their relative order
+
+        Constraints:
+        - In-place required (space constraint)
+        - 1 <= nums.length <= 10^4
+        - -2^31 <= nums[i] <= 2^31 - 1
+
+        Edge-cases:
+        - None
+
+        Approach 1:
+        - Instead of moving the zeroes to the left, we can also move the non-zero elements to the right
+        - Use slow/fast 2 pointers approach
+        - Slow pointer keeps track of first 0
+        - Fast pointer looks for non-zero elements
+        - Allows for one-pass
+
+        Time & Space Complexity (respectively):
+        - O(n), as we make one pass through the array
+        - O(1), as we make changes in place, not changing the size of the array or declaring new data structures
+        """
+        left = 0
+        for right in range(len(nums)):
+            if nums[right] != 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+        return nums
+
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    nums = [2,0,3,4,0,6]
+    print(solution.moveZeroes(nums))
